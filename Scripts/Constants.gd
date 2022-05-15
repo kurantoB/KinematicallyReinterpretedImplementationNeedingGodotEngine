cd30ce0e72059bd10e25116075d563ad26b0c5e7
kurantoB <laomeister93@gmail.com>
2022-05-15 16:46:30 -0700

    KRINGE

--- Scripts/Constants.gd
+++ Scripts/Constants.gd
@@ -1,48 +1,30 @@
 enum UnitType {
 	PLAYER,
-	TEST_ENEMY
 }
 
 enum ActionType {
-	CANCEL_FLYING,
-	CHANNEL,
-	CROUCH,
 	JUMP,
 	MOVE,
-	DASH,
-	DIGEST,
-	DROP_PORTING,
-	FLOAT,
 	RECOIL,
-	SLIDE,
-	DISCARD,
 }
 
 enum UnitCondition {
 	CURRENT_ACTION,
-	HAS_ABILITY,
 	IS_ON_GROUND,
-	IS_PORTING,
 	IS_RECOILING,
-	MOVING_STATUS
-	IS_INVINCIBLE,
+	MOVING_STATUS,
 	IS_GRAVITY_AFFECTED,
 }
 
 enum UnitCurrentAction {
 	IDLE,
-	CHANNELING,
-	CROUCHING,
 	JUMPING,
-	SLIDING,
-	FLYING,
 	RECOILING
 }
 
 enum UnitMovingStatus {
 	IDLE,
 	MOVING,
-	DASHING,
 }
 
 enum PlayerInput {
@@ -58,71 +40,10 @@ enum PlayerInput {
 
 const UNIT_TYPE_ACTIONS = {
 	UnitType.PLAYER: [
-		ActionType.CANCEL_FLYING,
-		ActionType.CHANNEL,
-		ActionType.CROUCH,
 		ActionType.JUMP,
 		ActionType.MOVE,
-		ActionType.DASH,
-		ActionType.DIGEST,
-		ActionType.DROP_PORTING,
-		ActionType.FLOAT,
 		ActionType.RECOIL,
-		ActionType.SLIDE,
-		ActionType.DISCARD,
 	],
-}
-
-const UNIT_TYPE_CURRENT_ACTIONS = {
-	UnitType.PLAYER: [
-		UnitCurrentAction.IDLE,
-		UnitCurrentAction.CHANNELING,
-		UnitCurrentAction.CROUCHING,
-		UnitCurrentAction.JUMPING,
-		UnitCurrentAction.SLIDING,
-		UnitCurrentAction.FLYING,
-		UnitCurrentAction.RECOILING,
-	],
-}
-
-const UNIT_TYPE_MOVING_STATUS = {
-	UnitType.PLAYER: [
-		UnitMovingStatus.IDLE,
-		UnitMovingStatus.MOVING,
-		UnitMovingStatus.DASHING
-	],
-}
-
-const UNIT_TYPE_CONDITIONS = {
-	UnitType.PLAYER: {
-		UnitCondition.CURRENT_ACTION: UNIT_TYPE_CURRENT_ACTIONS[UnitType.PLAYER][UnitCurrentAction.IDLE],
-		UnitCondition.HAS_ABILITY: false,
-		UnitCondition.IS_ON_GROUND: true,
-		UnitCondition.IS_PORTING: false,
-		UnitCondition.MOVING_STATUS: UNIT_TYPE_MOVING_STATUS[UnitType.PLAYER][UnitMovingStatus.IDLE],
-		UnitCondition.IS_INVINCIBLE: false,
-		UnitCondition.IS_GRAVITY_AFFECTED: true,
-	},
-}
-
-const PLAYER_TIMERS = {
-	ActionType.SLIDE: 0.5,
-	ActionType.FLOAT: 0.25,
-	ActionType.DASH: 0.3
-}
-
-const CURRENT_ACTION_TIMERS = {
-	UnitType.PLAYER: {
-		UnitCurrentAction.SLIDING: 1.5,
-		UnitCurrentAction.RECOILING: 1.5,
-		UnitCurrentAction.JUMPING: 0.75,
-	}
-}
-
-const UNIT_CONDITION_TIMERS = {
-	UnitType.PLAYER: {
-		UnitCondition.IS_INVINCIBLE: 3.0
-	}
 }
 
 enum DIRECTION {
@@ -132,19 +53,43 @@ enum DIRECTION {
 	RIGHT,
 }
 
+const UNIT_TYPE_CURRENT_ACTIONS = {
+	UnitType.PLAYER: [
+		UnitCurrentAction.IDLE,
+		UnitCurrentAction.JUMPING,
+		UnitCurrentAction.RECOILING,
+	],
+}
+
+const UNIT_TYPE_MOVING_STATUS = {
+	UnitType.PLAYER: [
+		UnitMovingStatus.IDLE,
+		UnitMovingStatus.MOVING,
+	],
+}
+
+const UNIT_TYPE_CONDITIONS = {
+	UnitType.PLAYER: {
+		UnitCondition.CURRENT_ACTION: UNIT_TYPE_CURRENT_ACTIONS[UnitType.PLAYER][UnitCurrentAction.IDLE],
+		UnitCondition.IS_ON_GROUND: true,
+		UnitCondition.MOVING_STATUS: UNIT_TYPE_MOVING_STATUS[UnitType.PLAYER][UnitMovingStatus.IDLE],
+		UnitCondition.IS_GRAVITY_AFFECTED: true,
+	},
+}
+
+const CURRENT_ACTION_TIMERS = {
+	UnitType.PLAYER: {
+		UnitCurrentAction.JUMPING: 0.75,
+	}
+}
+
 const ENV_COLLIDERS = {
 	UnitType.PLAYER: [
 		[Vector2(0, 1.5), [DIRECTION.LEFT, DIRECTION.UP, DIRECTION.RIGHT]],
 		[Vector2(0, .75), [DIRECTION.LEFT, DIRECTION.RIGHT]],
 		[Vector2(0, 0), [DIRECTION.LEFT, DIRECTION.DOWN, DIRECTION.RIGHT]],
 	],
-	UnitType.TEST_ENEMY: [
-		[Vector2(0, 1), [DIRECTION.LEFT, DIRECTION.UP, DIRECTION.RIGHT]],
-		[Vector2(0, .5), [DIRECTION.LEFT, DIRECTION.RIGHT]],
-		[Vector2(0, 0), [DIRECTION.LEFT, DIRECTION.DOWN, DIRECTION.RIGHT]],
-	],
 }
-const CROUCH_FACTOR = 0.67 # of total height
 
 const INPUT_MAP = {
 	PlayerInput.UP: "ui_up",
@@ -168,14 +113,25 @@ enum MAP_ELEM_TYPES {
 	LEDGE,
 }
 
+const TilesetMapElems = {
+	"TestTileSet": {
+		MAP_ELEM_TYPES.SQUARE: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+		MAP_ELEM_TYPES.SLOPE_LEFT: [15, 16],
+		MAP_ELEM_TYPES.SLOPE_RIGHT: [17, 18],
+		MAP_ELEM_TYPES.SMALL_SLOPE_LEFT_1: [9],
+		MAP_ELEM_TYPES.SMALL_SLOPE_LEFT_2: [10, 11],
+		MAP_ELEM_TYPES.SMALL_SLOPE_RIGHT_1: [12],
+		MAP_ELEM_TYPES.SMALL_SLOPE_RIGHT_2: [13, 14],
+		MAP_ELEM_TYPES.LEDGE: [19, 20, 21, 22],
+	}
+}
+
 const UnitSprites = {
 	UnitType.PLAYER: {
 		# "Sprite class": [is animation?, [Node list]]
-		"Idle": [true, ["Idle"]],
-		"Walk": [true, ["Walk"]],
-		"Dash": [true, ["Dash"]],
-		"Jump": [false, ["Jump1", "Jump2"]],
-		"Fly": [false, ["Fly1", "Fly2"]]
+		"Idle": [false, ["Idle"]],
+		"Walk": [false, ["Idle"]],
+		"Jump": [false, ["Idle"]],
 	}
 }
 
@@ -184,16 +140,12 @@ const MOVE_SPEEDS = {
 }
 
 const UNIT_TYPE_JUMP_SPEEDS = {
-	UnitType.PLAYER: 7
+	UnitType.PLAYER: 8
 }
 
 const SCALE_FACTOR = 3
-const GRID_SIZE = 20
-const GRAVITY = 20
-const MAX_FALL_SPEED = -10
-#const MAX_FALL_LITE = -4
-const MAX_FALL_LITE = -1
+const GRID_SIZE = 20 # pixels
+const GRAVITY = 35
+const MAX_FALL_SPEED = -14
 const ACCELERATION = 35
-const DASH_SPEED = 10
-const GRAVITY_LITE = 8
 const QUANTUM_DIST = 0.001
